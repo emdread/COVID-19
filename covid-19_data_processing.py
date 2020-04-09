5067312bc2d43a37c9c67ba92fe7ab565b88f6c2
Martin <martdr123@gmail.com>
2020-04-09 21:42:26 +1000

    Finally got google sheets functionality working

--- covid-19_data_processing.py
+++ covid-19_data_processing.py
@@ -1,9 +1,13 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+from dates import sheets_date
 import datetime
 
-print('test1')
+import requests
+from bs4 import BeautifulSoup
+
+# print('test1')
 
 # use credentials to create a client to interact with the Google Drive API
 scope = ['https://spreadsheets.google.com/feeds',
@@ -11,7 +15,7 @@ scope = ['https://spreadsheets.google.com/feeds',
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 
-print('test2')
+# print('test2')
 
 # Find the workbook by name and open the first sheet
 # sheet = client.open('COVID19_data').sheet1
@@ -19,7 +23,7 @@ print('test2')
 # Finding the workbook by key, because name wasn't working
 spreadsheet = client.open_by_key('1gqXuCcGYdXkpWMrDGwFbfQjSX5pui2QrhjsDeuJkpRM')
 
-print('test3')
+# print('test3')
 
 sheet = spreadsheet.sheet1
 
@@ -29,4 +33,51 @@ sheet = spreadsheet.sheet1
 #
 # print(cell.value)
 
-print(sheet.acell('B1', value_render_option='FORMULA').value)
+# print(sheet.acell('B1', value_render_option='FORMULA').value)
+# print(sheets_date(sheet.cell(2, 2, value_render_option='UNFORMATTED_VALUE').value))
+
+class Day:
+    """The class for each data point, a day"""
+
+    def __init___(self, date, home_cell):
+        """Takes a datetime object, and a cell object."""
+
+        self.date = date
+        self.home_cell = home_cell
+
+def initialise_data(sheet):
+    """Takes a worksheet object, and collects the data into a list of Day objects."""
+
+    # The initial cell which the data is relative to
+    init_cell = sheet.find('Day')
+
+    # The categories that each day will have
+    cats = []
+    # The cell below the initial cell
+    current_cell = sheet.cell(init_cell.row + 1, init_cell.col)
+    while current_cell.value:
+        cats.append(current_cell.value)
+        current_cell = sheet.cell(current_cell.row + 1, current_cell.col)
+
+    # The day objects already in the sheet
+    days = []
+    current_cell = sheet.cell(init_cell.row, init_cell.col + 1)
+    while current_cell.value:
+        days.append(Day(sheets_date(current_cell.value), current_cell))
+        current_cell = sheet.cell(current_cell.row, current_cell.col + 1)
+
+    return days
+
+def collect_links(url='https://www.dhhs.vic.gov.au/media-hub-coronavirus-disease-covid-19'):
+    """Takes a url string, and returns a list of all the links on that page"""
+
+    # Requests the html, and parses it to be readable
+    r = requests.get(url)
+    r_html = r.text
+    soup = BeautifulSoup(r_html, "html.parser")
+
+    link_list = []
+
+    for link in soup.find_all('a'):
+        print(link)
+        print()
--- dates.py
+++ dates.py
@@ -1,1 +1,27 @@
 import datetime
+
+
+# https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
+
+# datetime.datetime.strptime('13/3/2003', '%d/%m/%Y')
+# datetime.datetime(2003, 3, 13, 0, 0)
+
+# datetime.datetime(2003, 3, 13, 0, 0).strftime('%d/%m/%Y')
+# '13/03/2003'
+
+# datetime.datetime.strptime('13 March 2003', '%d %B %Y')
+# datetime.datetime(2003, 3, 13, 0, 0)
+
+# datetime.datetime(2003, 3, 13, 0, 0).strftime('%d %B %Y')
+#'13 March 2003'
+
+
+# Sheets stores dates as the number of days since 1/1/1900
+# So add 693596
+
+
+def sheets_date(date):
+    """Takes either an int, representing the number of days since 1/1/1900, which is how Google
+    Sheets stores dates, and converts it to a datetime object."""
+
+    return datetime.date.fromordinal(date + 693596)
